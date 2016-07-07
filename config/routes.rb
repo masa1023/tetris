87a1f6be8f9bcd77cb183d54f6e339a693607231
minamimasanobu <west013minami@gmail.com>
2016-07-07 19:49:02 +0900

    fix design, contact form, header

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root 'rooms#index'
-  resources :maps, only: :index
-  resources :books, only: :index
-  resources :contacts, only: :index
   resources :bookings, only: :index             # 入力画面
   post 'bookings/confirm' => 'bookings#confirm'   # 確認画面
   post 'bookings/thanks' => 'bookings#thanks'     # 送信完了画面
