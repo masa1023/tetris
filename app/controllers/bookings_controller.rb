f6da39a6a1c00d823580303b1366bf1f9b8c62e6
minamimasanobu <west013minami@gmail.com>
2016-07-09 03:02:33 +0900

    estimate function

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,11 +9,10 @@ class BookingsController < ApplicationController
   def confirm
     # 入力値のチェック
     @booking = Booking.new(params[:booking])
-    if @booking.valid?
-      # OK。確認画面を表示
+    if @booking.valid? # OK。確認画面を表示
+      @estimate = estimate(@booking)
       render :action => 'confirm'
-    else
-      # NG。入力画面を再表示
+    else # NG。入力画面を再表示
       render :action => 'retype'
     end
   end
@@ -21,7 +20,8 @@ class BookingsController < ApplicationController
   def thanks
     # メール送信
     @booking = Booking.new(params[:booking])
-    BookingMailer.received_email(@booking).deliver
+    @estimate = estimate(@booking)
+    BookingMailer.received_email(@booking, @estimate).deliver
     # 完了画面を表示
     render :action => 'thanks'
   end
@@ -32,4 +32,28 @@ class BookingsController < ApplicationController
     render :action => 'retype'
   end
 
+  def estimate(booking)
+
+    number_of_people = booking.number_of_people.to_i #宿泊人数
+    period = booking.period.to_i #宿泊日数
+    monthly_rent = 30000 #家賃/月
+    daily_rent = 1000 #家賃/日
+    monthly_utility = 9000 #共益費/月
+    daily_utility = monthly_utility / 30 #共益費/日
+    commission = 10000 #入居手数料
+
+    if period < 30
+      4000 * period
+    else
+      total_rent = monthly_rent * (period / 30) + daily_rent * (period % 30)
+      total_utility = monthly_utility * (period / 30) + daily_utility * (period % 30)
+      (total_rent + total_utility + commission) * number_of_people
+    end
+
+  end
 end
+
+
+
+
+
--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -2,8 +2,9 @@ class BookingMailer < ActionMailer::Base
   default from: "west013minami@gmail.com"   # 送信元アドレス
   default to: "west013minami@gmail.com"     # 送信先アドレス
 
-  def received_email(booking)
+  def received_email(booking, estimate)
     @booking = booking
+    @estimate = estimate
     mail(to: @booking.email, bcc: ["takknnjp@yahoo.co.jp", "west013minami@gmail.com"], subject: 'お問い合わせを承りました')
   end
 end
