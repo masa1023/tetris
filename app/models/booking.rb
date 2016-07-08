4a0e9f0ea4b1f2f67dfbdee195f6d95a923d9874
minamimasanobu <west013minami@gmail.com>
2016-07-08 23:21:17 +0900

    fix text(adress) contact form bug, mail to takeshi and guest

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,13 +1,13 @@
 class Booking
   include ActiveModel::Model
 
-  attr_accessor :name, :email, :age, :sex, :nationality, :number, :bed_type, :checkin_date, :period, :preview_date, :message
+  attr_accessor :name, :email, :age, :sex, :nationality, :number_of_people, :bed_type, :checkin_date, :period, :preview_date, :message
 
   validates :name, :presence => {:message => '名前を入力してください'}
   validates :email, :presence => {:message => 'メールアドレスを入力してください'}
   validates :age, :presence => {:message => '年齢を入力してください'}
   validates :sex, :presence => {:message => '性別を入力してください'}
   validates :nationality, :presence => {:message => '国籍を入力してください'}
-  validates :number, :presence => {:message => '宿泊予定人数を入力してください'}
+  validates :number_of_people, :presence => {:message => '宿泊予定人数を入力してください'}
   validates :bed_type, :presence => {:message => '希望ベッドタイプしてください'}
 end
